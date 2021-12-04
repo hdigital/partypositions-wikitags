002c573b8d0c3b825e54b64d451bfe787201a450
Michael Herrmann <michael.herrmann@uni.kn>
2021-12-04 12:40:16 +0100

    Stan estimation
    major
    
    * adapt estimation parameters (=> thinning unnecessary in Stan)
    * adapt file names (=> all output as csv; one file per chain)
    * auto detect cores for estimation
    * adapt inits (=> Stan picks RNGs and chain seeds)
    * adapt data and ensure no conversion of data types by Stan is needed
    * adapt estimation function
    * identify and extract party and tag positions from stan_out
    * adapt storing of party and tag positions
    
    minor
    
    * edit: code description

--- 03-estimation/02-estimation-jags.R
+++ 03-estimation/02-estimation-jags.R
@@ -1,95 +1,107 @@
 ## Estimation of left-right positions from tags (ideology and position).
-## The Jags model is only estimated if no RData in sub-folder "estimation-models"
-## exist. Remove Rdata file to start estimation -- see below 
+## The Stan model is only estimated if no csv file with MCMC samples 
+## exists in sub-folder "estimation-models". Remove csv files to start 
+## estimation -- see below 
 
 
 library(readr)
-library(jagsUI)
+library(rstan)
 
 
 wp_model <- 2          # model to estimate -- M1 or M2
 
 n_chains <- 4          # number of Markov chains
-n_iter <- 25000        # total number of iterations
-n_burn <- 2500         # number of burn-in iterations
-n_thin <- 10           # keep every nth iteration
-run_parallel <- TRUE   # run chains in parallel
+n_iter <- 2500         # total number of iterations
+n_burn <- 250          # number of burn-in iterations
+stan_seed <- 123456
 
 
-file_jags_results <- sprintf(
-  "03-estimation/estimation-model/02-result-m%d.RData", 
-  wp_model
-)
+## Stan config ----
+
+options(mc.cores = parallel::detectCores())
+
+
+## Files input/output ----
+
 file_position_out <- "03-estimation/estimation-results/positions-%s-m%d.csv"
 
+file_stan <- sprintf("03-estimation/model-m%d.stan", wp_model)
+files_stan_samples <- sprintf(
+  "03-estimation/estimation-model/02-stan-samples-m%d_%d.csv", 
+  wp_model, 
+  seq_len(n_chains)
+)
+filename_stan_samples <- unique(gsub("_[[:digit:]]*.csv", "", files_stan_samples))
+filename_stan_diags <- sprintf(
+  "03-estimation/estimation-model/02-stan-diags-m%d",
+  wp_model
+)
 
 
-## Declare JAGS input ----
+## Stan input ----
 
-load(sprintf("03-estimation/estimation-model/01-data-m%d.RData", wp_model))
+load(sprintf("03-estimation/estimation-model/01-data-m%d-stan.RData", wp_model))
 
 param_save <- c("a", "b", "o", "x")
 
-jags_inits <- list(
+stan_inits <- list(
   delta = ab_init[, "alpha"] - abs(ab_init[, "beta"]) * o_init ^ 2,
   lambda = abs(ab_init[, "beta"]) * o_init * 2,
   beta = abs(ab_init[, "beta"]),
   xstar = x_init
 )
-inits_add <- list()
 
-rng_names <- c(
-  "Wichmann-Hill", 
-  "Marsaglia-Multicarry", 
-  "Super-Duper", 
-  "Mersenne-Twister"
-)
-rng_chain <- rep_len(paste0("base::", rng_names), n_chains)
-
-jags_data <- list(
-  y_ideo = ideology,
-  D = dim(ideology),
-  con = which(names(ideology) == "conservatism")
+stan_data <- list(
+  con = which(levels(ideology$tag) == "conservatism"),
+  I = length(party),
+  J = nlevels(ideology$tag),
+  N = nrow(ideology),
+  party_ideo = ideology$party,
+  tag = as.integer(ideology$tag),
+  y_ideo = as.integer(ideology$outcome)
 )
 
 if (wp_model == 2) {
   
-  param_save <- c(c("g", "t"), param_save)
-  inits_add <- list(gamma = g_init, tau = t_init)
-  jags_data <- append(jags_data, list(y_lr = left_right, N = nrow(left_right)))
+  param_save <- c("g", "t", param_save)
+  stan_inits <- append(stan_inits, list(gamma = g_init, tau = t_init))
+  data_add <- list(
+    M = nrow(left_right), 
+    K = length(unique(left_right$outcome)),
+    party_lr = left_right$party,
+    y_lr = as.integer(left_right$outcome)
+  )
+  stan_data <- append(stan_data, data_add)
   
 }
 
-jags_inits <- lapply(
-  seq_len(n_chains), 
-  function(.x) append(
-    append(jags_inits, inits_add),
-    list(".RNG.name" = rng_chain[.x], ".RNG.seed" = .x * 10)
-  )
+stan_inits <- setNames(
+  rep(list(stan_inits), n_chains), 
+  paste("chain", seq_len(n_chains), sep = "_")
 )
 
 
+# Run Stan ----
 
-## Run JAGS ----
-
-if (file.exists(file_jags_results)) {
+if (any(file.exists(files_stan_samples))) {
   
-  load(file_jags_results)
+  stan_out <- read_stan_csv(files_stan_samples)
   
 } else {
   
-  jags_out <- jags(
-    data = jags_data,
-    inits = jags_inits,
-    parameters.to.save = param_save,
-    model.file = sprintf("03-estimation/model-m%d-jags.txt", wp_model),
-    n.chains = n_chains,
-    parallel = run_parallel,
-    n.iter = n_iter,
-    n.burnin = n_burn,
-    n.thin = n_thin
+  stan_out <- stan(
+    file = file_stan,
+    data = stan_data,
+    iter = n_iter,
+    warmup = n_burn,
+    chains = n_chains,
+    init = stan_inits,
+    pars = param_save,
+    include = TRUE,
+    seed = stan_seed,
+    sample_file = filename_stan_samples,
+    diagnostic_file = filename_stan_diags
   )
-  save(jags_out, partyfacts_id, file = file_jags_results)
   
 }
 
@@ -97,18 +109,23 @@ if (file.exists(file_jags_results)) {
 
 ## Export position estimates (and 95% CIs) to csv ----
 
+posterior_stats <- summary(stan_out, pars = param_save)$summary
+
+x <- grep("x\\[[[:digit:]]*\\]$", rownames(posterior_stats), value = TRUE)
+o <- grep("o\\[[[:digit:]]*\\]$", rownames(posterior_stats), value = TRUE)
+
 pos_parties <- data.frame(
-  partyfacts_id = partyfacts_id,
-  x = apply(jags_out$sims.list$x, 2, median),
-  lo = apply(jags_out$sims.list$x, 2, quantile, 0.025),
-  up = apply(jags_out$sims.list$x, 2, quantile, 0.975)
+  partyfacts_id = names(party),
+  x = posterior_stats[x, "50%"],
+  lo = posterior_stats[x, "2.5%"],
+  up = posterior_stats[x, "97.5%"]
 )
 write_csv(pos_parties, sprintf(file_position_out, "parties", wp_model))
 
 pos_tags <- data.frame(
-  ideology = names(ideology),
-  o = apply(jags_out$sims.list$o, 2, median),
-  lo = apply(jags_out$sims.list$o, 2, quantile, 0.025),
-  up = apply(jags_out$sims.list$o, 2, quantile, 0.975)
+  ideology = levels(ideology$tag),
+  o = posterior_stats[o, "50%"],
+  lo = posterior_stats[o, "2.5%"],
+  up = posterior_stats[o, "97.5%"]
 )
 write_csv(pos_tags, sprintf(file_position_out, "tags", wp_model))
