7534ad99cec9eae682f2ece2469cc22c45785637
Michael Herrmann <michael.herrmann@uni.kn>
2021-12-04 15:44:56 +0100

    run all scripts
    * adapt descriptions: JAGS -> Stan
    * adapt extensions of result files: RData -> csv
    * adapt names of result files

--- z-run-all.R
+++ z-run-all.R
@@ -1,10 +1,10 @@
 # run all scripts (R and Python) -- see README.md
-# time intense steps (get Wikipedia data, Jags estimation) are optional
+# time intense steps (get Wikipedia data, Stan estimation) are optional
 
 library(callr)
 library(fs)
 library(tidyverse)
-# packages required by some scripts: ggrepel jagsUI 
+# packages required by some scripts: ggrepel rstan 
 
 
 option_update_data <- FALSE
@@ -24,7 +24,7 @@ if(option_update_data) {
   system("cd 01-data-sources/02-wikipedia/ && python3 z-get-partyfacts-data.py")
   # update Wikipedia data -- needs Python and packages
   system("cd 01-data-sources/02-wikipedia/ && python3 z-run-wikipedia-scripts.py")
-
+  
   # extract party position from various sources (raw data not in Git)
   if(dir_exists("01-data-sources/03-party-positions/00-sources-raw/")) {
     rscript("01-data-sources/03-party-positions/01-sources-select.R")
@@ -40,11 +40,11 @@ rscripts_dir("02-data-preparation")
 
 ## Estimation and validation ----
 
-# estimate models (default M2 only) -- time intense (12h) and requiring JAGS
+# estimate models (default M2 only) -- time intense (2.5h) and requiring Stan
 if(option_run_estimation) {
-  # model is only estimated if estimation results (*.RData) are removed
-  file_delete(dir_ls("03-estimation/estimation-model/", glob = "*.RData"))
-
+  # model is only estimated if estimation results (*.csv) are removed
+  file_delete(dir_ls("03-estimation/estimation-model/", glob = "*.csv"))
+  
   rscripts_dir("03-estimation")
   r(function() rmarkdown::render("03-estimation/03-convergence.Rmd"))
 }
@@ -75,7 +75,7 @@ files_data <-
   filter(
     str_detect(path, "(csv|RData|html|zip)$"),
     str_detect(path, "data-files-doc|00-sources-raw|zyx", negate = TRUE)
-    ) %>% 
+  ) %>% 
   select(data_file=path, last_modified=modification_time)
 
 # get documentation of datasets
@@ -93,5 +93,7 @@ cat("\n\nLast Wikipedia data extraction (needs running Python scripts)")
 file.info("01-data-sources/02-wikipedia/wikipedia-data/01-wp-data-json.zip")$mtime
 file.info("01-data-sources/02-wikipedia/wikipedia-data/03-infobox-tags.csv")$mtime
 
-cat("\nLast model JAGS estimation (only run if no estimation output exists)\n")
-file.info("03-estimation/estimation-model/02-result-m2.RData")$mtime
+cat("\nLast model Stan estimation (only run if no estimation output exists)\n")
+file.info(
+  list.files("03-estimation/estimation-model", pattern = "csv", full.names = TRUE)
+)$mtime
